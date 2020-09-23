fe242146f3a385416e8c9ec9c3874c5c83b20a19
hongdae <fholng@outlook.com>
2020-09-24 03:29:18 +0900

    d

--- Ex05/src/main/webapp/resources/js/reply.js
+++ Ex05/src/main/webapp/resources/js/reply.js
@@ -1,21 +1,21 @@
 console.log("Reply Module........");
 
-var replyService = (function() {
+var replyService = (function () {
 
 	function add(reply, callback, error) {
 		console.log("add reply...............");
 
 		$.ajax({
-			type : 'post',
-			url : '/replies/new',
-			data : JSON.stringify(reply),
-			contentType : "application/json; charset=utf-8",
-			success : function(result, status, xhr) {
+			type: 'post',
+			url: '/replies/new',
+			data: JSON.stringify(reply),
+			contentType: "application/json; charset=utf-8",
+			success: function (result, status, xhr) {
 				if (callback) {
 					callback(result);
 				}
 			},
-			error : function(xhr, status, er) {
+			error: function (xhr, status, er) {
 				if (error) {
 					error(er);
 				}
@@ -23,55 +23,55 @@ var replyService = (function() {
 		})
 	}
 
-//	function getList(param, callback, error) {
-//
-//		var bno = param.bno;
-//		var page = param.page || 1;
-//
-//		$.getJSON("/replies/pages/" + bno + "/" + page + ".json",
-//				function(data) {
-//					if (callback) {
-//						callback(data);
-//					}
-//				}).fail(function(xhr, status, err) {
-//			if (error) {
-//				error();
-//			}
-//		});
-//	}
-	
-	
+	//	function getList(param, callback, error) {
+	//
+	//		var bno = param.bno;
+	//		var page = param.page || 1;
+	//
+	//		$.getJSON("/replies/pages/" + bno + "/" + page + ".json",
+	//				function(data) {
+	//					if (callback) {
+	//						callback(data);
+	//					}
+	//				}).fail(function(xhr, status, err) {
+	//			if (error) {
+	//				error();
+	//			}
+	//		});
+	//	}
+
+
 
 	function getList(param, callback, error) {
 
-	    var bno = param.bno;
-	    var page = param.page || 1;
-	    
-	    $.getJSON("/replies/pages/" + bno + "/" + page + ".json",
-	        function(data) {
-	    	
-	          if (callback) {
-	            //callback(data); // 댓글 목록만 가져오는 경우 
-	            callback(data.replyCnt, data.list); //댓글 숫자와 목록을 가져오는 경우 
-	          }
-	        }).fail(function(xhr, status, err) {
-	      if (error) {
-	        error();
-	      }
-	    });
-	  }
+		var bno = param.bno;
+		var page = param.page || 1;
 
-	
+		$.getJSON("/replies/pages/" + bno + "/" + page + ".json",
+			function (data) {
+
+				if (callback) {
+					//callback(data); // 댓글 목록만 가져오는 경우 
+					callback(data.replyCnt, data.list); //댓글 숫자와 목록을 가져오는 경우 
+				}
+			}).fail(function (xhr, status, err) {
+				if (error) {
+					error();
+				}
+			});
+	}
+
+
 	function remove(rno, callback, error) {
 		$.ajax({
-			type : 'delete',
-			url : '/replies/' + rno,
-			success : function(deleteResult, status, xhr) {
+			type: 'delete',
+			url: '/replies/' + rno,
+			success: function (deleteResult, status, xhr) {
 				if (callback) {
 					callback(deleteResult);
 				}
 			},
-			error : function(xhr, status, er) {
+			error: function (xhr, status, er) {
 				if (error) {
 					error(er);
 				}
@@ -84,16 +84,16 @@ var replyService = (function() {
 		console.log("RNO: " + reply.rno);
 
 		$.ajax({
-			type : 'put',
-			url : '/replies/' + reply.rno,
-			data : JSON.stringify(reply),
-			contentType : "application/json; charset=utf-8",
-			success : function(result, status, xhr) {
+			type: 'put',
+			url: '/replies/' + reply.rno,
+			data: JSON.stringify(reply),
+			contentType: "application/json; charset=utf-8",
+			success: function (result, status, xhr) {
 				if (callback) {
 					callback(result);
 				}
 			},
-			error : function(xhr, status, er) {
+			error: function (xhr, status, er) {
 				if (error) {
 					error(er);
 				}
@@ -103,13 +103,13 @@ var replyService = (function() {
 
 	function get(rno, callback, error) {
 
-		$.get("/replies/" + rno + ".json", function(result) {
+		$.get("/replies/" + rno + ".json", function (result) {
 
 			if (callback) {
 				callback(result);
 			}
 
-		}).fail(function(xhr, status, err) {
+		}).fail(function (xhr, status, err) {
 			if (error) {
 				error();
 			}
@@ -131,27 +131,27 @@ var replyService = (function() {
 			var mi = dateObj.getMinutes();
 			var ss = dateObj.getSeconds();
 
-			return [ (hh > 9 ? '' : '0') + hh, ':', (mi > 9 ? '' : '0') + mi,
-					':', (ss > 9 ? '' : '0') + ss ].join('');
+			return [(hh > 9 ? '' : '0') + hh, ':', (mi > 9 ? '' : '0') + mi,
+				':', (ss > 9 ? '' : '0') + ss].join('');
 
 		} else {
 			var yy = dateObj.getFullYear();
 			var mm = dateObj.getMonth() + 1; // getMonth() is zero-based
 			var dd = dateObj.getDate();
 
-			return [ yy, '/', (mm > 9 ? '' : '0') + mm, '/',
-					(dd > 9 ? '' : '0') + dd ].join('');
+			return [yy, '/', (mm > 9 ? '' : '0') + mm, '/',
+				(dd > 9 ? '' : '0') + dd].join('');
 		}
 	}
 	;
 
 	return {
-		add : add,
-		get : get,
-		getList : getList,
-		remove : remove,
-		update : update,
-		displayTime : displayTime
+		add: add,
+		get: get,
+		getList: getList,
+		remove: remove,
+		update: update,
+		displayTime: displayTime
 	};
 
 })();
